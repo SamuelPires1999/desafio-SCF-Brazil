6bfab543be9df1fbb8294179b0e6e1c3ba6e4711
Samuel Pires <samuel.ferpires@gmail.com>
2023-06-11 19:01:16 -0300

    teste3: DONE

--- teste3.js
+++ teste3.js
@@ -1,15 +1,27 @@
-var data =  require("./fakeData");
+const data =  require("./fakeData");
 
 module.exports = function(req, res) {
   
-    var name =  req.query.name;
+    const name =  req.query.name;
 
-    for(let i = 0; i < data.length;  i++) {
-        if(i.name == name) {
-            data[i] = null;
-        }
+    // for(let i = 0; i < data.length;  i++) {
+    //     if(i.name == name) {
+    //         data[i] = null;
+    //     }
+    // }
+
+    // Primeiro achamos a posicao onde o objeto contendo este nome esta posicionado.
+    const index = data.indexOf(data.find(item => item.name === name));
+
+    // Apos isso vemos se a posicao e maior que -1 (sinalizando que o elemento existe)
+    if (index > -1) { 
+        data.splice(index, 1); // Aqui utilizamos Splice para retirar o item em dada posicao.
+    } else {
+        // Caso o index nao seja encontrado, retornamos a mensagem Item not found
+        return res.send("Item not found")
     }
 
-    res.send("success");
+
+    return res.send("success");
 
 };
